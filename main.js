5903d0c1b0fe131325ea73a6c3556bfe48b84022
Konstantin <grigo.ko2004@gmail.com>
2025-12-12 19:40:47 +0500

    update mobile button menu

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const burger = document.getElementById("burger");
-    const menu = document.querySelector(".mobile-menu");
+    const menu   = document.querySelector(".mobile-menu");
 
-    burger.addEventListener("change", () => {
-        menu.classList.toggle("menu-open", burger.checked);
+    // Показываем/скрываем меню при клике
+    burger.addEventListener("click", () => {
+        menu.classList.toggle("menu-open");
     });
 
+    // Автоматически скрываем меню, если ширина > 1024
     function checkScreenWidth() {
         if (window.innerWidth > 1024) {
             menu.classList.remove("menu-open");
-            burger.checked = false;
         }
     }
+    checkScreenWidth();
 
     // Проверка перевода вкл/выкл
     function isRussian(){
@@ -26,6 +28,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function init(){
         checkScreenWidth();
+        document.querySelectorAll('.uc-notranslate').forEach(el => {
+            el.classList.add('notranslate');
+        });
     }
 
     window.addEventListener("resize", checkScreenWidth);
